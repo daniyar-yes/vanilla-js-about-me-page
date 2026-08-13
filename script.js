6947b23d3fe8d9647be1a65163efb3126ac6d692
daniyar-yes <yeskaliyevdan@hotmail.com>
2026-08-12 18:03:16 -0700

    add checbox, class update on checked, emoji for delete

--- script.js
+++ script.js
@@ -3,32 +3,58 @@ const inputEntry = document.querySelector('#item-entry')
 const buttonAdd = document.querySelector('#btn-add-to-do')
 const list = document.querySelector('#to-do-list')
 
+let idCounter = 1;
 
-buttonAdd.addEventListener('click', addItemHandler);
+
+buttonAdd.addEventListener('click', function (event) {
+    if (inputEntry.value === '') {
+        console.warn('From BUTTON: empty string, returning');
+        return;
+    }
+    addItemHandler();
+});
+//how can we guard the btn add event listener from firing with empty strings
 
 inputEntry.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
+        if (inputEntry.value === '') {
+            console.warn('From ENTER: empty string, returning');
+            return;
+        };
         addItemHandler();
     }
 
 })
 
 function addItemHandler() {
-    if (inputEntry.value == '') return; // early return
-
+    if (inputEntry.value === '') {
+        console.warn('Last guard from function: empty string, returning');
+        return;
+    };
     const listItem = document.createElement('li');
-    listItem.textContent = inputEntry.value;
+    listItem.id = `list-item-${idCounter}`
+    idCounter = idCounter + 1;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `my-checkbox-${idCounter}`;
+    checkbox.addEventListener('click', () => {
+        listItem.classList.toggle('checked-item');
+    })
 
     const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete'
+    deleteButton.textContent = '🗑️'
 
+    listItem.textContent = inputEntry.value;
+
+    listItem.appendChild(checkbox);
     listItem.appendChild(deleteButton);
+
     list.appendChild(listItem);
 
     deleteButton.addEventListener('click', () => {
         listItem.remove()
     })
-
 
     inputEntry.value = '';
     inputEntry.focus();
